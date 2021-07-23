66f10348a16fed0e97ad143c81dd68404294e402
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2021-07-23 16:02:21 +0700

    Bird's Nest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -199,44 +199,50 @@
                     <p>Terima kasih telah menghubungi kami</p>
         		</div>
                 <div class="row text-center">
-                    <div class="col-lg-4 col-md-12">
-                        <div>
-                            <svg class="feather feather-map-pin" xmlns="http://www.w3.org/2000/svg" width="50"
-                                height="50" viewBox="0 0 24 24" fill="none" stroke="#A40603" stroke-width="1"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                <circle cx="12" cy="10" r="3"></circle>
-                            </svg>
-                            <h4 class="mt-5">Alamat :</h4>
-                            <span class="text-black">Jl. Ketintang No.169, Ketintang, Kec. Wonokromo, Kota SBY, Jawa Timur 60231</span>
-                        </div>
+                    <div class="col-lg-4 col-md-12 mb-5">
+
+                                <div class="px-5">
+                                    <svg class="feather feather-map-pin" xmlns="http://www.w3.org/2000/svg" width="50"
+                                        height="50" viewBox="0 0 24 24" fill="none" stroke="#A40603" stroke-width="1"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                                        <circle cx="12" cy="10" r="3"></circle>
+                                    </svg>
+                                    <h4 class="mt-5">Alamat :</h4>
+                                    <span class="text-black">Jl. Ketintang No.169, Ketintang, Kec. Wonokromo, Kota SBY, Jawa Timur 60231</span>
+                                </div>
+
                     </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div>
-                            <svg class="feather feather-mail" xmlns="http://www.w3.org/2000/svg" width="50"
-                                height="50" viewBox="0 0 24 24" fill="none" stroke="#A40603" stroke-width="1"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path
-                                    d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
-                                </path>
-                                <polyline points="22,6 12,13 2,6"></polyline>
-                            </svg>
-                            <h4 class="mt-5">Email Us</h4>
-                            <a href="mailto:master.dundee@gmail.com"> master.dundee@gmail.com</a>
-                        </div>
+                    <div class="col-lg-4 col-md-12 mb-5">
+
+                                <div class="mt-4">
+                                    <svg class="feather feather-mail" xmlns="http://www.w3.org/2000/svg" width="50"
+                                        height="50" viewBox="0 0 24 24" fill="none" stroke="#A40603" stroke-width="1"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path
+                                            d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                                        </path>
+                                        <polyline points="22,6 12,13 2,6"></polyline>
+                                    </svg>
+                                    <h4 class="mt-5">Email Us</h4>
+                                    <a href="mailto:master.dundee@gmail.com"> master.dundee@gmail.com</a>
+                                </div>
+
                     </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div>
-                            <svg class="feather feather-phone-call" xmlns="http://www.w3.org/2000/svg" width="50"
-                                height="50" viewBox="0 0 24 24" fill="none" stroke="#A40603" stroke-width="1"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path
-                                    d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
-                                </path>
-                            </svg>
-                            <h4 class="mt-5">Nomor Telp</h4>
-                            <a href="https://wa.me/6281332666688?text=Halo%20kak,%20Apakah%20Bird's%20Nest%20masih%20tersedia%20?">+62 813 3266 6688</a>
-                        </div>
+                    <div class="col-lg-4 col-md-12 mb-5">
+
+                                <div class="mt-4">
+                                    <svg class="feather feather-phone-call" xmlns="http://www.w3.org/2000/svg" width="50"
+                                        height="50" viewBox="0 0 24 24" fill="none" stroke="#A40603" stroke-width="1"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path
+                                            d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                                        </path>
+                                    </svg>
+                                    <h4 class="mt-5">Nomor Telp</h4>
+                                    <a href="https://wa.me/6281332666688?text=Halo%20kak,%20Apakah%20Bird's%20Nest%20masih%20tersedia%20?">+62 813 3266 6688</a>
+                                </div>
+
                     </div>
                 </div>
             </div>
